cc2775246ebdc8870bfc3c7413468778890e5400
Paul Lam <paul@quantisan.com>
2015-06-13 11:53:24 -0700

    move /data-release-demo to root

--- src/clj/privatise_data_release/routes/home.clj
+++ src/clj/privatise_data_release/routes/home.clj
@@ -2,6 +2,7 @@
   (:require [hiccup.page :refer [html5 include-js include-css]]
             [hiccup.element :refer [javascript-tag link-to mail-to]]
 
+            [ring.util.response :refer [redirect]]
             [compojure.core :refer [defroutes GET]]
             [environ.core :refer [env]]))
 
@@ -24,39 +25,6 @@
 
      body]))
 
-(def home-page
-  (html5
-    [:html {:lang "en"}
-     [:head
-      [:meta {:charset "utf-8"}]
-      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
-      [:meta {:name "viewport"
-              :content "width=device-width, initial-scale=1"}]
-
-      [:title "Masking.io | Simple data masking"]
-      [:meta {:name :description :content ""}]
-      [:meta {:name :author :content "masking.io"}]
-
-      (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css")
-      (include-css (if (env :dev?) "css/site.css" "css/site.min.css"))]
-
-     [:body
-      [:div.container
-       [:div.header.clearfix
-        [:h3.text-muted "Masking.io"]]
-       [:div.jumbotron.accent
-        [:h2 "Simple data masking"]
-        [:br]
-        [:br]
-        [:br]
-        [:p
-         (link-to {:class "typeform-share link btn btn-lg btn-success" :data-mode "1" :target "_blank"}
-                  "https://quantisan.typeform.com/to/HNCBxS"
-                  "Sign up for updates")
-         (javascript-tag "(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()")]
-        [:br]
-        [:small (mail-to {} "info@masking.io")]]]]]))
-
 (def data-release-demo
   (base-template
     [:body
@@ -69,6 +37,6 @@
      (include-js "js/app.js")]))
 
 (defroutes home-routes
-  (GET "/" [] home-page)
-  (GET "/data-release-demo" [] data-release-demo))
+  (GET "/" [] data-release-demo)
+  (GET "/data-release-demo" [] (redirect "/")))
 
